b0c3aa2e3aa6e377efe27759a5fd42e29898aa3e
kaptikova <anna.kaptikova@gmail.com>
2024-04-14 16:32:10 +0200

    Added tests

--- tests.spec.js
+++ tests.spec.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 import { expect } from 'chai'
 
+const authorizationToken = 'ghp_0000000000DUMMY-TOKEN000000000000000'
+const authorizationHeader = `Bearer ${authorizationToken}`
+
 
 describe("Test_1 Check repo", () => {
 
@@ -16,7 +19,7 @@ describe("Test_1 Check repo", () => {
                 url: 'https://api.github.com/user/starred',
                 headers: {
                     'X-GitHub-Api-Version': '2022-11-28',
-                    'Authorization': `Bearer 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`,
+                    'Authorization': authorizationToken,
                 },
                 owner: 'OWNER',
                 repo: 'REPO'
@@ -26,7 +29,7 @@ describe("Test_1 Check repo", () => {
             console.log('Test error get user: ', error);
         }
 
-        console.log('Test response get user: ', response.data);
+        //console.log('Test response get user: ', response.data);
 
         // Check that staus code 200
         expect(response.status).equals(200);
@@ -38,7 +41,7 @@ describe("Test_1 Check repo", () => {
 
         //Check that response contain array
         expect(Array.isArray(response.data)).to.be.true;
-        console.log("it is rue or not ");
+        console.log("it is true or not ");
 
         //Check that arrain contain not more than 10 records
         expect(response.data.length).to.be.at.most(10);
@@ -47,7 +50,7 @@ describe("Test_1 Check repo", () => {
         //Check response's structure
         response.data.forEach(repo => {
             const namePropery = expect(repo).to.have.property('name');
-            console.log("data to have name Property: ", namePropery);
+            console.log("data to have name Property1: ", namePropery);
 
             const descriptionPropery = expect(repo).to.have.property('description');
             console.log("data to have description Property: ", descriptionPropery);
@@ -79,7 +82,7 @@ describe("Test_2 Check that User added star for repo", () => {
                 url: 'https://api.github.com/user/starred',
                 headers: {
                     'X-GitHub-Api-Version': '2022-11-28',
-                    'Authorization': `Bearer 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`,
+                    'Authorization': authorizationToken,
                 },
                 owner: 'OWNER',
                 repo: 'REPO'
@@ -91,7 +94,7 @@ describe("Test_2 Check that User added star for repo", () => {
 
         response.data.forEach(repo => {
         const namePropery = expect(repo).to.have.property('name');
-        console.log("data to have name Property: ", namePropery);
+        console.log("data to have name Property2: ", namePropery);
         });
 
     });
@@ -99,7 +102,7 @@ describe("Test_2 Check that User added star for repo", () => {
     it("PUT /user/starred", async () => {
         console.log('Test start add star');
 
-        let response = null;
+        //let response = null;
 
         try {
 
@@ -108,7 +111,7 @@ describe("Test_2 Check that User added star for repo", () => {
                 url: 'https://api.github.com/user/starred/kaptikova/new',
                 headers: {
                     'X-GitHub-Api-Version': '2022-11-28',
-                    'Authorization': `Bearer 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`,
+                    'Authorization': authorizationToken,
                 },
                 owner: 'OWNER',
                 repo: 'REPO'
@@ -129,7 +132,7 @@ describe("Test_2 Check that User added star for repo", () => {
                 url: 'https://api.github.com/user/starred',
                 headers: {
                     'X-GitHub-Api-Version': '2022-11-28',
-                    'Authorization': `Bearer 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`,
+                    'Authorization': authorizationToken,
                 },
                 owner: 'OWNER',
                 repo: 'REPO'
@@ -141,15 +144,16 @@ describe("Test_2 Check that User added star for repo", () => {
 
         response.data.forEach(repo => {
         const namePropery = expect(repo).to.have.property('name');
-        console.log("data to have name Property: ", namePropery);
+        console.log("data to have name Property3: ", namePropery);
         });
 
 });
 
 });
+
 
 describe("Test_3 Check that User can delete a ster for repo", () => {
-    let response = null;
+  
 
     it("DELETE /user/starred", async () => {
         console.log('Test start add star');
@@ -163,7 +167,7 @@ describe("Test_3 Check that User can delete a ster for repo", () => {
                 url: 'https://api.github.com/user/starred/kaptikova/new',
                 headers: {
                     'X-GitHub-Api-Version': '2022-11-28',
-                    'Authorization': `Bearer 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`,
+                    'Authorization': authorizationToken,
                 },
                 owner: 'OWNER',
                 repo: 'REPO'
@@ -176,6 +180,9 @@ describe("Test_3 Check that User can delete a ster for repo", () => {
     });
 
     it("GET", async () => {
+        
+        let response = null;
+
         console.log('Check that repo "new" dont have a star');
         try {
 
@@ -184,7 +191,7 @@ describe("Test_3 Check that User can delete a ster for repo", () => {
                 url: 'https://api.github.com/user/starred',
                 headers: {
                     'X-GitHub-Api-Version': '2022-11-28',
-                    'Authorization': `Bearer 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`,
+                    'Authorization': authorizationToken,
                 },
                 owner: 'OWNER',
                 repo: 'REPO'
@@ -196,62 +203,90 @@ describe("Test_3 Check that User can delete a ster for repo", () => {
 
         response.data.forEach(repo => {
         const namePropery = expect(repo).to.have.property('name');
-        console.log("data to have name Property: ", namePropery);
+        console.log("data to have name Property4: ", namePropery);
         });
 
 });
 });
+
 
 describe("Test_4 Check negative scenario", () => {
-    let response = null;
+    
 
-    it("DELETE /user/starred", async () => {
-        console.log('Test start add star');
+    it("Check that User can not login with incorrect token", async () => {
+        console.log('GET/uer/incorrect token');
 
         let response = null;
 
         try {
 
             response = await axios({
-                method: 'delete',
+                method: 'get',
                 url: 'https://api.github.com/user/starred/kaptikova/new',
                 headers: {
                     'X-GitHub-Api-Version': '2022-11-28',
                     'Authorization': `Bearer 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`,
                 },
                 owner: 'OWNER',
-                repo: 'REPO'
+                repo: 'REPO',
+
             });
 
+            console.log('Test error incorrect token1: ', error);
+
         } catch (error) {
-            console.log('Test error get user: ', error);
+            console.log('Test error incorrect token2: ', error);
         }           
 
     });
 
-    it("GET", async () => {
-        console.log('Check that repo "new" dont have a star');
+
+    it("Check that User can not delete a star if repo does not have one", async () => {
+        console.log('Delete/uer/incorrect repo');
+
+        let response = null;
+
         try {
 
             response = await axios({
                 method: 'get',
-                url: 'https://api.github.com/user/starred',
+                url: 'https://api.github.com/user/starred/kaptikova/helloci',
                 headers: {
                     'X-GitHub-Api-Version': '2022-11-28',
-                    'Authorization': `Bearer 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`,
+                    'Authorization': authorizationToken,
                 },
                 owner: 'OWNER',
                 repo: 'REPO'
             });
 
         } catch (error) {
-            console.log('Test error get user: ', error);
-        }
+            console.log('Test error delete incorrect repo: ', error);
+        }           
 
-        response.data.forEach(repo => {
-        const namePropery = expect(repo).to.have.property('name');
-        console.log("data to have name Property: ", namePropery);
-        });
+    });
 
-});
+    it("Check that User can not check have or not pero a star if repo incorrect", async () => {
+        console.log('GET/uer/incorrect repo');
+
+        let response = null;
+
+        try {
+
+            response = await axios({
+                method: 'get',
+                url: 'https://api.github.com/user/starred/kaptikova/HTT',
+                headers: {
+                    'X-GitHub-Api-Version': '2022-11-28',
+                    'Authorization': authorizationToken,
+                },
+                owner: 'OWNER',
+                repo: 'REPO'
+            });
+
+        } catch (error) {
+            console.log('Test error get incorrect repo: ', error);
+        }           
+
+    });
+
 });
